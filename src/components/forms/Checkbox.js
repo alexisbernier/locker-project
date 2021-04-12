b9a661722c9c4fc10b19ade5f46b0f4da05c4915
AlexisBernier1 <alexisbernierpro1@gmail.com>
2021-04-12 11:49:46 +0200

    feat: changed  Checkbox component style

--- src/components/forms/Checkbox.js
+++ src/components/forms/Checkbox.js
@@ -1,12 +1,25 @@
 import Form from "react-bootstrap/Form";
+import styled from "@emotion/styled";
+
+const StyledForm = styled.form`
+  font-family: Georgia, serif;
+  margin-left: auto;
+  margin-right: auto;
+  width: 30%;
+  height: 100%;
+`;
 
 const Checkbox = () => {
   return (
-    <Form>
+    <StyledForm>
       <Form.Group controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="" />
+        <Form.Check
+          required
+          label="Agree to Terms of service"
+          feedback="You must agree before confirming"
+        />
       </Form.Group>
-    </Form>
+    </StyledForm>
   );
 };
 
