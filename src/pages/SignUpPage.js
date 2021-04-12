8a4eb5d553a08a62a5f4909fde60e331711cf8da
AlexisBernier1 <alexisbernierpro1@gmail.com>
2021-04-12 11:50:20 +0200

    feat: changed SignUpPage structure

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,20 +1,25 @@
 import EmailForm from "../components/forms/EmailForm";
 import PasswordForm from "../components/forms/PasswordForm";
 import ConfirmPasswordForm from "../components/forms/ConfirmPasswordForm";
-import Checkbox from "../components/forms/Checkbox";
 import UserNameForm from "../components/forms/UserNameForm";
 import Footer from "../components/Footer";
+import Checkbox from "../components/forms/Checkbox";
+import Btn from "../components/buttons/Button";
+import styled from "@emotion/styled";
+
+const Container = styled.form``;
 
 const SignUpPage = () => {
   return (
-    <>
+    <Container>
       <UserNameForm />
       <EmailForm />
       <PasswordForm />
       <ConfirmPasswordForm />
       <Checkbox />
+      <Btn />
       <Footer />
-    </>
+    </Container>
   );
 };
 
