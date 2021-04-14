88b2176ecb07b34a5070ea2665c45662a596eecc
AlexisBernier1 <alexisbernierpro1@gmail.com>
2021-04-14 16:38:49 +0200

    feat: added Community component

---
+++ src/components/Community.js
@@ -0,0 +1,16 @@
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  position: fixed;
+  text-align: right;
+`;
+
+const Community = () => {
+  return (
+    <>
+      <Container>qwqwqw</Container>
+    </>
+  );
+};
+
+export default Community;
