4102ed9ce8bd66843456ee4ff8efa203dea14b25
AlexisBernier1 <alexisbernierpro1@gmail.com>
2021-04-14 11:31:31 +0200

    fix: fixed SignUpPage

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -5,15 +5,12 @@ import UserNameForm from "../components/forms/UserNameForm";
 import Footer from "../components/Footer";
 import Checkbox from "../components/forms/Checkbox";
 import Btn from "../components/buttons/Button";
-import styled from "@emotion/styled";
 import SiteName from "../components/SiteName";
 import Nav from "../components/Navbar";
 
-const Container = styled.form``;
-
 const SignUpPage = () => {
   return (
-    <Container>
+    <>
       <SiteName />
       <Nav />
       <UserNameForm />
@@ -23,7 +20,7 @@ const SignUpPage = () => {
       <Checkbox />
       <Btn />
       <Footer />
-    </Container>
+    </>
   );
 };
 
