f5669a651d05932c1d7a949b4438d40f1a64b5db
AlexisBernier1 <alexisbernierpro1@gmail.com>
2021-04-14 11:32:43 +0200

    update: updated HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,15 @@
 import Footer from "../components/Footer";
+import Nav from "../components/Navbar";
+import SiteName from "../components/SiteName";
 
 const HomePage = () => {
-  return <Footer />;
+  return (
+    <>
+      <Footer />
+      <SiteName />
+      <Nav />
+    </>
+  );
 };
 
 export default HomePage;
