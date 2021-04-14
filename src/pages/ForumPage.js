c0dc4712e56e0e61da8bf0509ec054d06a4ea094
AlexisBernier1 <alexisbernierpro1@gmail.com>
2021-04-14 14:36:52 +0200

    update: updated ForumPage

--- src/pages/ForumPage.js
+++ src/pages/ForumPage.js
@@ -1,5 +1,15 @@
+import SiteName from "../components/SiteName";
+import Nav from "../components/Navbar";
 import Footer from "../components/Footer";
 
 const ForumPage = () => {
-  return <Footer />;
+  return (
+    <>
+      <SiteName />
+      <Nav />
+      <Footer />
+    </>
+  );
 };
+
+export default ForumPage;
