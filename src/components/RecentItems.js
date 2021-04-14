bd65e75ca171fae62ad69d9040a53c504badf5bc
AlexisBernier1 <alexisbernierpro1@gmail.com>
2021-04-14 16:39:25 +0200

    feat: added RecentItems component

---
+++ src/components/RecentItems.js
@@ -0,0 +1,23 @@
+import Item from "./Item";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+
+const RecentItems = () => {
+  return (
+    <Container fluid>
+      <h3>Recent items</h3>
+      <Row>
+        <Item />
+        <Item />
+        <Item />
+      </Row>
+      <Row>
+        <Item />
+        <Item />
+        <Item />
+      </Row>
+    </Container>
+  );
+};
+
+export default RecentItems;
