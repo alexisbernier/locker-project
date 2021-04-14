fc809d6ac3a7a1ca6e0467bcc41129b091b665eb
AlexisBernier1 <alexisbernierpro1@gmail.com>
2021-04-14 16:55:00 +0200

    update: updated About

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+const About = () => {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus
+        reprehenderit alias enim, nobis iure est inventore, nihil eligendi
+        voluptatum autem quibusdam aspernatur sint atque debitis magni a ducimus
+        dolores odit doloremque corrupti obcaecati? Doloribus laudantium
+        architecto, qui reprehenderit veniam, corporis nostrum, numquam dicta
+        eligendi voluptatibus quos ad veritatis earum rerum.
+      </p>
+    </div>
+  );
+};
+
+export default About;
