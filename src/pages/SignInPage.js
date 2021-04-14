0dcec1ef3cc8cfb458ab27cb922da69792b6ad94
AlexisBernier1 <alexisbernierpro1@gmail.com>
2021-04-14 11:32:22 +0200

    update: updated SignInPage

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -2,10 +2,14 @@ import EmailForm from "../components/forms/EmailForm";
 import PasswordForm from "../components/forms/PasswordForm";
 import Button from "../components/buttons/Button";
 import Footer from "../components/Footer";
+import SiteName from "../components/SiteName";
+import Nav from "../components/Navbar";
 
 const SignInPage = () => {
   return (
     <>
+      <SiteName />
+      <Nav />
       <EmailForm />
       <PasswordForm />
       <Button />
