f40457ff77dadee8e2ad8e2e40b7d16c306d16ee
AlexisBernier1 <alexisbernierpro1@gmail.com>
2021-04-15 20:03:45 +0200

    update: updated ItemPage

--- src/pages/ItemPage.js
+++ src/pages/ItemPage.js
@@ -1,13 +1,25 @@
 import Footer from "../components/Footer";
 import Nav from "../components/Navbar";
 import SiteName from "../components/SiteName";
+import Item from "../components/Item";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  margin: 2rem;
+`;
 
 const ItemPage = () => {
   return (
     <>
       <SiteName />
       <Nav />
-      <Footer />
+      <Container>
+        <Item />
+        <div>
+          <h3>Estimated price : 123$</h3>
+        </div>
+        <Footer />
+      </Container>
     </>
   );
 };
