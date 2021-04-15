612ec0f2d2ec8306a57a371b5d6944545300db93
AlexisBernier1 <alexisbernierpro1@gmail.com>
2021-04-15 20:04:08 +0200

    update: updated HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,17 +1,24 @@
-import About from "../components/About";
 import Footer from "../components/Footer";
 import Nav from "../components/Navbar";
+// import ProfilePreview from "../components/ProfilePreview";
 import RecentItems from "../components/RecentItems";
 import SiteName from "../components/SiteName";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  margin: 2rem;
+`;
 
 const HomePage = () => {
   return (
     <>
       <SiteName />
       <Nav />
-      <About />
-      <RecentItems />
-      <Footer />
+      <Container>
+        {/* <ProfilePreview /> */}
+        <RecentItems />
+        <Footer />
+      </Container>
     </>
   );
 };
