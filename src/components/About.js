aaf71148006a5c4ca4040f59d40c765b5701cc97
AlexisBernier1 <alexisbernierpro1@gmail.com>
2021-04-15 11:40:28 +0200

    update: added style to About component

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,21 @@
+import styled from "@emotion/styled";
+
+const StyledContainer = styled.div`
+  border: 1px solid;
+  margin: 0;
+  padding: 0;
+`;
+
 const About = () => {
   return (
-    <div>
+    <StyledContainer>
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus
         reprehenderit alias enim, nobis iure est inventore, nihil eligendi
         voluptatum autem quibusdam aspernatur sint atque debitis magni a ducimus
-        dolores odit doloremque corrupti obcaecati? Doloribus laudantium
-        architecto, qui reprehenderit veniam, corporis nostrum, numquam dicta
-        eligendi voluptatibus quos ad veritatis earum rerum.
+        dolores odit doloremque corrupti obcaecati?
       </p>
-    </div>
+    </StyledContainer>
   );
 };
 
