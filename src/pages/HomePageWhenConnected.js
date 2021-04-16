70ab6f044988d2683f2a30d022fe82cae6d23eeb
AlexisBernier1 <alexisbernierpro1@gmail.com>
2021-04-17 00:50:47 +0200

    fix: fixed imports

--- src/pages/HomePageWhenConnected.js
+++ src/pages/HomePageWhenConnected.js
@@ -1,7 +1,7 @@
 import Footer from "../components/Footer";
 import RecentItems from "../components/RecentItems";
-import styled from "@emotion/styled";
 import NavCo from "../components/NavbarWhenConnected";
+import styled from "@emotion/styled";
 
 const Container = styled.div`
   margin: 2rem;
