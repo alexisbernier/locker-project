f59d93301b71550db8a1d2103b00c2e069d6f5f6
AlexisBernier1 <alexisbernierpro1@gmail.com>
2021-04-17 00:49:53 +0200

    fix: fixed imports

--- src/components/ProfilePreview.js
+++ src/components/ProfilePreview.js
@@ -1,5 +1,4 @@
-import Container from "react-bootstrap/Container";
-import Col from "react-bootstrap/Col";
+import { Container, Col } from "react-bootstrap/";
 import ProfilePicture from "../components/ProfilePicture";
 
 const ProfilePreview = () => {
