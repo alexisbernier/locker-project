8fe0a183c662f9397b2ab6bc833159260e23d859
AlexisBernier1 <alexisbernierpro1@gmail.com>
2021-04-16 11:27:15 +0200

    update: removed SiteName component

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -5,13 +5,11 @@ import UserNameForm from "../components/forms/UserNameForm";
 import Footer from "../components/Footer";
 import Checkbox from "../components/forms/Checkbox";
 import Btn from "../components/buttons/Button";
-import SiteName from "../components/SiteName";
 import Nav from "../components/Navbar";
 
 const SignUpPage = () => {
   return (
     <>
-      <SiteName />
       <Nav />
       <UserNameForm />
       <EmailForm />
