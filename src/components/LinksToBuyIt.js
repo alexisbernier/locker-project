4451bccfc4994f4de7b78126cd2d0f2d796541f7
AlexisBernier1 <alexisbernierpro1@gmail.com>
2021-04-17 00:49:46 +0200

    fix: fixed imports

--- src/components/LinksToBuyIt.js
+++ src/components/LinksToBuyIt.js
@@ -1,6 +1,5 @@
 import Item from "./Item";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
+import { Container, Row } from "react-bootstrap";
 
 const LinksToBuyIt = () => {
   return (
