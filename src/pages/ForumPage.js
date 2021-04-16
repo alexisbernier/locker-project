ffeae1e51a64e4e2fba07449c3959425fecd9e2f
AlexisBernier1 <alexisbernierpro1@gmail.com>
2021-04-16 11:26:47 +0200

    update: removed SiteName component

--- src/pages/ForumPage.js
+++ src/pages/ForumPage.js
@@ -1,11 +1,9 @@
-import SiteName from "../components/SiteName";
 import Nav from "../components/Navbar";
 import Footer from "../components/Footer";
 
 const ForumPage = () => {
   return (
     <>
-      <SiteName />
       <Nav />
       <Footer />
     </>
