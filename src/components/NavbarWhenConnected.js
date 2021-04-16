b394f69ef8037047dfe0e00e6d1e2c2161e1f0d3
AlexisBernier1 <alexisbernierpro1@gmail.com>
2021-04-16 16:01:02 +0200

    feat: added nav that appears when user connected

---
+++ src/components/NavbarWhenConnected.js
@@ -0,0 +1,43 @@
+const NavCo = () => {
+  return (
+    <>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light mb-5">
+        <a class="navbar-brand" href="/">
+          Locker
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="/">
+                Home <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="/ForumPage">
+                Forum
+              </a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="/ProfilePage">
+                Profile
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default NavCo;
