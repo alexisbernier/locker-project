ad407f44e5f3c8c99acc9346fed1b9a6ce47e97f
AlexisBernier1 <alexisbernierpro1@gmail.com>
2021-04-17 16:27:40 +0200

    feat: added topicpage to route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SignUpPage from "./pages/SignUpPage";
 import SignInPage from "./pages/SignInPage";
 import HomePage from "./pages/HomePage";
 import ForumPage from "./pages/ForumPage";
+import TopicPage from "./pages/TopicPage";
 import ItemPage from "./pages/ItemPage";
 import ProfilePage from "./pages/ProfilePage";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomePageCo from "./pages/HomePageWhenConnected";
 import BoxPage from "./pages/BoxPage";
 
@@ -21,6 +22,9 @@ const App = () => {
         </Route>
         <Route path="/ForumPage">
           <ForumPage />
+        </Route>
+        <Route path="/TopicPage">
+          <TopicPage />
         </Route>
         <Route path="/ProfilePage">
           <ProfilePage />
