abca20454607b40a58f1743f2879939c0f3139b7
AlexisBernier1 <alexisbernierpro1@gmail.com>
2021-04-17 16:27:31 +0200

    update: update forumpage

--- src/pages/ForumPage.js
+++ src/pages/ForumPage.js
@@ -1,10 +1,80 @@
-import Nav from "../components/Navbar";
+import NavCo from "../components/NavbarWhenConnected";
 import Footer from "../components/Footer";
+
+import { Container, Row, Col, Table, Button } from "react-bootstrap";
 
 const ForumPage = () => {
   return (
     <>
-      <Nav />
+      <NavCo />
+      <Container className="pr-5">
+        <Row>
+          <Col sm={9}>
+            <Button variant="secondary">Create a Topic</Button>
+            <Table striped bordered hover size="md">
+              <tbody>
+                <tr>
+                  <td>
+                    <a href="/TopicPage">Topic</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Ipsum</td>
+                </tr>
+                <tr>
+                  <td>Lorem</td>
+                </tr>
+                <tr>
+                  <td>Ipsum</td>
+                </tr>
+                <tr>
+                  <td>Lorem</td>
+                </tr>
+                <tr>
+                  <td>Ipsum</td>
+                </tr>
+                <tr>
+                  <td>Lorem</td>
+                </tr>
+                <tr>
+                  <td>Ipsum</td>
+                </tr>
+              </tbody>
+            </Table>
+          </Col>
+          <Col sm={3}>
+            <p>Popular posts</p>
+            <Table striped bordered hover size="md">
+              <tbody>
+                <tr>
+                  <td>Lorem</td>
+                </tr>
+                <tr>
+                  <td>Ipsum</td>
+                </tr>
+                <tr>
+                  <td>Lorem</td>
+                </tr>
+                <tr>
+                  <td>Ipsum</td>
+                </tr>
+                <tr>
+                  <td>Lorem</td>
+                </tr>
+                <tr>
+                  <td>Ipsum</td>
+                </tr>
+                <tr>
+                  <td>Lorem</td>
+                </tr>
+                <tr>
+                  <td>Ipsum</td>
+                </tr>
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+      </Container>
       <Footer />
     </>
   );
