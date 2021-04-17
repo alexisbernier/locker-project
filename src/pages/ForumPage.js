f00bb5c59dc1315121ae73a030208eacd8788446
AlexisBernier1 <alexisbernierpro1@gmail.com>
2021-04-17 16:29:06 +0200

    update: update forumpage

--- src/pages/ForumPage.js
+++ src/pages/ForumPage.js
@@ -10,7 +10,9 @@ const ForumPage = () => {
       <Container className="pr-5">
         <Row>
           <Col sm={9}>
-            <Button variant="secondary">Create a Topic</Button>
+            <Button variant="secondary" className="mb-3">
+              Create a Topic
+            </Button>
             <Table striped bordered hover size="md">
               <tbody>
                 <tr>
