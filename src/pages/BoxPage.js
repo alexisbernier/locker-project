07f6e482fad0a2b51328bb559890416fb118a431
AlexisBernier1 <alexisbernierpro1@gmail.com>
2021-04-17 16:01:44 +0200

    update: updated boxpage

--- src/pages/BoxPage.js
+++ src/pages/BoxPage.js
@@ -1,7 +1,56 @@
+import { Container, Row, Col } from "react-bootstrap";
+
 import Footer from "../components/Footer";
+import Item from "../components/Item";
+import NavCo from "../components/NavbarWhenConnected";
 
 const BoxPage = () => {
-  return <Footer />;
+  return (
+    <>
+      <NavCo />
+      <Container>
+        <p>My Collection</p>
+        <Row className="no-gutters">
+          <Col sm={8}>
+            <Row>
+              <Col>
+                <Item />
+              </Col>
+              <Col>
+                <Item />
+              </Col>
+              <Col>
+                <Item />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Item />
+              </Col>
+              <Col>
+                <Item />
+              </Col>
+              <Col>
+                <Item />
+              </Col>
+            </Row>
+          </Col>
+          <Col sm={4}>
+            <p>Estimated Value: 890$</p>
+            <p className="mb-5">Number of Items: 255</p>
+            <p>Items im looking for:</p>
+            <Row>
+              <Col>
+                <Item />
+                <Item />
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+      <Footer />
+    </>
+  );
 };
 
 export default BoxPage;
