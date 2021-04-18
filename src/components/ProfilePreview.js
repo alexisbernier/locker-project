764b391d4566753ec7adccf65661d2d32ed2043d
AlexisBernier1 <alexisbernierpro1@gmail.com>
2021-04-18 15:48:28 +0200

    update: updated profilepreview component

--- src/components/ProfilePreview.js
+++ src/components/ProfilePreview.js
@@ -4,8 +4,11 @@ import ProfilePicture from "../components/ProfilePicture";
 const ProfilePreview = () => {
   return (
     <Container>
-      <Col>
+      <Col className="mt-5">
         <ProfilePicture />
+        <div className="mb-5">
+          <h2 class="text-justify">Username</h2>
+        </div>
       </Col>
     </Container>
   );
