15d21ecb68691618f70970947cea9565dbbb13c1
Avétis KAZARIAN <avetis.k@gmail.com>
2021-04-19 15:20:58 +0200

    chore: general clean up and fix

--- src/pages/ItemPage.js
+++ src/pages/ItemPage.js
@@ -1,6 +1,5 @@
 import Nav from "../components/Navbar";
 import Item from "../components/Item";
-import ItemDescription from "../components/ItemDescription";
 import LinksToBuyIt from "../components/LinksToBuyIt";
 
 import { Container, Row, Col } from "react-bootstrap";
@@ -18,8 +17,19 @@ const ItemPage = () => {
             </div>
           </Col>
           <Col>
-            <div className="mb-5">
-              <ItemDescription />
+            <div className="mb-5 border bg-light px-3 py-2">
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Delectus reprehenderit alias enim, nobis iure est inventore,
+                nihil eligendi voluptatum autem quibusdam aspernatur sint atque
+                debitis magni a ducimus dolores odit doloremque corrupti
+                obcaecati? Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Molestias, suscipit nostrum ab, aperiam sint qui soluta
+                ullam quasi, distinctio quaerat aut inventore ipsum quam
+                reiciendis iste illo adipisci recusandae doloribus harum
+                architecto. Mollitia aliquid atque quidem sequi vero eaque.
+                Explicabo!
+              </p>
             </div>
             <LinksToBuyIt />
           </Col>
